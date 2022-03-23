7785cb00cf509e7cb584feda7e149ab5bf57529a
ZalEmperon <flowgreen173@gmail.com>
2022-03-23 20:56:53 +0700

    Sel

---
+++ routes/index.js
@@ -0,0 +1,147 @@
+var express = require('express');
+var router = express.Router();
+var session_store;
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.redirect('/stokmobil');
+});
+
+router.get('/customers', function(req, res, next) {
+	res.redirect('/customers');
+});
+
+router.get('/login',function(req,res,next){
+	res.render('main/login',{title:"Login Page"});
+});
+
+router.get('/register',function(req,res,next){
+	res.render('main/register',{title:"Register Page"});
+});
+
+router.post("/register", function (req, res, next) {
+  req.assert("name", "Please fill the name").notEmpty();
+	req.assert("email", "Please fill the email").notEmpty();
+	req.assert("phone", "Please fill the phone").notEmpty();
+	req.assert("password", "Please fill the password").notEmpty();
+  var errors = req.validationErrors();
+  if (!errors) {
+    v_name = req.sanitize("name").escape().trim();
+    v_email = req.sanitize("email").escape().trim();
+    v_password = req.sanitize("password").escape().trim();
+    v_phone = req.sanitize("phone").escape();
+
+    var user = {
+      name: v_name,
+      password: v_password,
+      email: v_email,
+      phone: v_phone,
+    };
+
+    var insert_sql = "INSERT INTO user SET ?";
+    req.getConnection(function (err, connection) {
+      var query = connection.query(
+        insert_sql,
+        user,
+        function (err, result) {
+          if (err) {
+            var errors_detail = ("Error Insert : %s ", err);
+            req.flash("msg_error", errors_detail);
+            res.render("main/register", {
+              name: req.param("name"),
+              password: req.param("password"),
+              email: req.param("email"),
+              phone: req.param("phone"),
+              session_store: req.session,
+            });
+          } else {
+            req.flash("msg_info", "Create user success");
+            res.redirect("/login");
+          }
+        }
+      );
+    });
+  } else {
+    console.log(errors);
+    errors_detail = "<p>Sory there are error</p><ul>";
+    for (i in errors) {
+      error = errors[i];
+      errors_detail += "<li>" + error.msg + "</li>";
+    }
+    errors_detail += "</ul>";
+    req.flash("msg_error", errors_detail);
+    res.render("main/register", {
+      name: req.param("name"),
+      password: req.param("password"),
+      session_store: req.session,
+    });
+  }
+});
+
+router.post('/login',function(req,res,next){
+	session_store=req.session;
+	req.assert('email', 'Please fill the Username').notEmpty();
+	req.assert('email', 'Email not valid').isEmail();
+	req.assert('password', 'Please fill the Password').notEmpty();
+	var errors = req.validationErrors();
+	if (!errors) {
+		req.getConnection(function(err,connection){
+			pass = req.body.password; 
+			email = req.body.email;
+			
+			var query = connection.query('select * from user where email="'+email+'" and password="'+pass+'"',function(err,rows)
+			{
+				if(err)
+				{
+					var errornya  = ("Error Selecting : %s ",err.code );  
+					console.log(err.code);
+					req.flash('msg_error', errornya); 
+					res.redirect('/login'); 
+				}else
+				{
+					if(rows.length <=0)
+					{
+
+						req.flash('msg_error', "Wrong email address or password. Try again."); 
+						res.redirect('/login');
+					}
+					else
+					{	
+						session_store.is_login = true;
+						session_store.email = req.body.email;
+						res.redirect('/stokmobil');
+					}
+				}
+
+			});
+		});
+	}
+	else
+	{
+		errors_detail = "<p>Sory there are error</p><ul>";
+		for (i in errors) 
+		{ 
+			error = errors[i]; 
+			errors_detail += '<li>'+error.msg+'</li>'; 
+		} 
+		errors_detail += "</ul>"; 
+		console.log(errors_detail);
+		req.flash('msg_error', errors_detail); 
+		res.redirect('/login'); 
+	}
+});
+
+router.get('/logout', function(req, res)
+{ 
+	req.session.destroy(function(err)
+	{ 
+		if(err)
+		{ 
+			console.log(err); 
+		} 
+		else
+		{ 
+			res.redirect('/stokmobil'); 
+		} 
+	}); 
+});
+module.exports = router;
---
+++ routes/stokmobil.js
@@ -0,0 +1,242 @@
+var express = require("express");
+var router = express.Router();
+var http = require("http");
+var fs = require("fs");
+var fileUpload = require('express-fileupload');
+var path = require('path');
+var formidable = require("formidable");
+const check = require('express-validator/check').check;
+const validationResult = require('express-validator/check').validationResult;
+var mv = require("mv");
+var authentication_mdl = require("../middlewares/authentication");
+var session_store;
+/* GET Customer page. */
+
+router.get("/", function (req, res, next) {
+  req.getConnection(function (err, connection) {
+    var query = connection.query(
+      "SELECT DISTINCT * FROM stokmobil",
+      function (err, rows) {
+        if (err) var errornya = ("Error Selecting : %s ", err);
+        req.flash("msg_error", errornya);
+        res.render("stokmobil/list", {
+          title: "Stok Mobil",
+          data: rows,
+          session_store: req.session,
+        });
+      }
+    );
+  });
+});
+
+router.get("/cari/(:tipe)", function (req, res, next) {
+  req.getConnection(function (err, connection) {
+    var query = connection.query(
+      "SELECT * FROM stokmobil where tipe_mobil= ?",req.params.tipe,
+      function (err, rows) {
+        if (err) var errornya = ("Error Selecting : %s ", err);
+        req.flash("msg_error", errornya);
+        res.render("stokmobil/list", {
+          title: "Stok Mobil",
+          data: rows,
+          session_store: req.session,
+        });
+      }
+    );
+  });
+});
+
+router.delete("/delete/(:id)", authentication_mdl.is_login, function (req, res, next) {
+  req.getConnection(function (err, connection) {
+    var stokmobilid = {
+      id: req.params.id,
+    };
+
+    var delete_sql = "delete from stokmobil where ?";
+    req.getConnection(function (err, connection) {
+      var query = connection.query(
+        delete_sql,
+        stokmobilid,
+        function (err, result) {
+          if (err) {
+            var errors_detail = ("Error Delete : %s ", err);
+            req.flash("msg_error", errors_detail);
+            res.redirect("/stokmobil");
+          } else {
+            req.flash("msg_info", "Data Mobil Berhasil Dihapus");
+            res.redirect("/stokmobil");
+          }
+        }
+      );
+    });
+  });
+});
+
+router.get("/edit/(:id)", authentication_mdl.is_login, function (req, res, next) {
+  req.getConnection(function (err, connection) {
+    var query = connection.query(
+      "SELECT * FROM stokmobil where id=" + req.params.id,
+      function (err, rows) {
+        if (err) {
+          var errornya = ("Error Selecting : %s ", err);
+          req.flash("msg_error", errors_detail);
+          res.redirect("/stokmobil");
+        } else {
+          if (rows.length <= 0) {
+            req.flash("msg_error", "Stok Mobil tidak ditemukan");
+            res.redirect("/stokmobil");
+          } else {
+            console.log(rows);
+            res.render("stokmobil/edit", {
+              title: "Edit ",
+              data: rows[0],
+              session_store: req.session,
+            });
+          }
+        }
+      }
+    );
+  });
+});
+
+router.put("/edit/(:id)", authentication_mdl.is_login, function (req, res, next) {
+  req.assert("nama_mobil", "Isi Nama Mobil").notEmpty();
+  var errors = req.validationErrors();
+  if (!errors) {
+    v_mobil = req.sanitize("nama_mobil").escape().trim();
+    v_jumlah = req.sanitize("jumlah_mobil").escape().trim();
+    v_tipe = req.sanitize("tipe_mobil").escape().trim();
+    v_harga = req.sanitize("harga_mobil").escape().trim();
+    
+    if (!req.files) {
+      var stokmobilid = {
+        nama_mobil: v_mobil,
+        jumlah_mobil: v_jumlah,
+        tipe_mobil: v_tipe,
+        harga_mobil: v_harga
+      };
+    }else{
+      var file = req.files.gambar_mobil;
+      file.mimetype == "image/jpeg";
+      file.mv("public/images/upload/" + file.name);
+
+      var stokmobilid = {
+        nama_mobil: v_mobil,
+        jumlah_mobil: v_jumlah,
+        tipe_mobil: v_tipe,
+        harga_mobil: v_harga,
+        gambar_mobil: file.name,
+      };
+    }
+    
+    var update_sql = "update stokmobil SET ? where id = " + req.params.id;
+    req.getConnection(function (err, connection) {
+      var query = connection.query(
+        update_sql,
+        stokmobilid,
+        function (err, result) {
+          if (err) {
+            var errors_detail = ("Error Update : %s ", err);
+            req.flash("msg_error", errors_detail);
+            res.render("stokmobil/edit", {
+              nama_mobil: req.param("nama_mobil"),
+              jumlah_mobil: req.param("jumlah_mobil"),
+              tipe_mobil: req.param("tipe_mobil"),
+              harga_mobil: req.param("harga_mobil"),
+              gambar_mobil: req.param("gambar_mobil"),
+            });
+          } else {
+            req.flash("msg_info", "Update stokmobil success");
+            res.redirect("/stokmobil");
+          }
+        }
+      );
+    });
+  } else {
+    console.log(errors);
+    errors_detail = "<p>Sory there are error</p><ul>";
+    for (i in errors) {
+      error = errors[i];
+      errors_detail += "<li>" + error.msg + "</li>";
+    }
+    errors_detail += "</ul>";
+    req.flash("msg_error", errors_detail);
+    res.redirect("/stokmobil/edit/" + req.params.id);
+  }
+});
+
+router.post("/add", authentication_mdl.is_login, function (req, res, next) {
+  req.assert("nama_mobil", "Please fill the name").notEmpty();
+  var errors = req.validationErrors();
+  if (!errors) {
+    v_mobil = req.sanitize("nama_mobil").escape().trim();
+    v_jumlah = req.sanitize("jumlah_mobil").escape().trim();
+    v_tipe = req.sanitize("tipe_mobil").escape().trim();
+    v_harga = req.sanitize("harga_mobil").escape().trim();
+
+    var file = req.files.gambar_mobil;
+    file.mimetype == "image/jpeg";
+    file.mv("public/images/upload/" + file.name);
+    
+    var stokmobilid = {
+      nama_mobil: v_mobil,
+      jumlah_mobil: v_jumlah,
+      tipe_mobil: v_tipe,
+      harga_mobil: v_harga,
+      gambar_mobil: file.name,
+    };
+
+    var insert_sql = "INSERT INTO stokmobil SET ?";
+    req.getConnection(function (err, connection) {
+      var query = connection.query(
+        insert_sql,
+        stokmobilid,
+        function (err, result) {
+          if (err) {
+            var errors_detail = ("Error Insert : %s ", err);
+            req.flash("msg_error", errors_detail);
+            res.render("stokmobil/add-stokmobil", {
+              nama_mobil: req.param("nama_mobil"),
+              jumlah_mobil: req.param("jumlah_mobil"),
+              tipe_mobil: req.param("tipe_mobil"),
+              harga_mobil: req.param("harga_mobil"),
+              gambar_mobil: req.param("gambar_mobil"),
+              session_store: req.session,
+            });
+          } else {
+            req.flash("msg_info", "Create stokmobil success");
+            res.redirect("/stokmobil");
+          }
+        }
+      );
+    });
+  } else {
+    console.log(errors);
+    errors_detail = "<p>Sory there are error</p><ul>";
+    for (i in errors) {
+      error = errors[i];
+      errors_detail += "<li>" + error.msg + "</li>";
+    }
+    errors_detail += "</ul>";
+    req.flash("msg_error", errors_detail);
+    res.render("stokmobil/add-stokmobil", {
+      nama_mobil: req.param("nama_mobil"),
+      jumlah_mobil: req.param("jumlah_mobil"),
+      session_store: req.session,
+    });
+  }
+});
+
+router.get("/add", authentication_mdl.is_login, function (req, res, next) {
+  res.render("stokmobil/add-stokmobil", {
+    title: "Add New stokmobil",
+    nama_mobil: "",
+    jumlah_mobil: "",
+    tipe_mobil: "",
+    harga_mobil: "",
+    gambar_mobil: "",
+    session_store: req.session,
+  });
+});
+
+module.exports = router;
